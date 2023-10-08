094bed9ff96ada66d905a30ea1ab6c634424c5aa
Awenega <f.sudoso@gmail.com>
2023-10-08 11:51:02 +0200

    Fixed result None from parsing a refund

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from finances import get_refundEventList, parse_refund
 from datetime import datetime, timezone, timedelta
+import requests
+import json
 
 def main():
 
-    startDate = (((datetime(2023,8, 1)).replace(hour=0, minute=0, second=0, microsecond=0)).astimezone(timezone.utc)).isoformat()
-    endDate = ((datetime(2023,9,15)).astimezone(timezone.utc)).isoformat()
+    startDate = (((datetime(2023,7, 3)).replace(hour=0, minute=0, second=0, microsecond=0)).astimezone(timezone.utc)).isoformat()
+    endDate = ((datetime(2023,10,1)).astimezone(timezone.utc)).isoformat()
 
-    resi = []
+    refunds = []
     refundEventList = get_refundEventList(startDate, endDate)
-    for refund in refundEventList:
-        resi.append(parse_refund(refund))  
+    for refundEvent in refundEventList:
+        refund = parse_refund(refundEvent)
+        if refund is not None:
+            refunds.append(refund)
 
 if __name__ == "__main__":
     main()
